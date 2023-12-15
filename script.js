37ed3369a609ee5ad0a335b99572c1e1fc486213
M-Abeer <102969595+M-Abeer@users.noreply.github.com>
2023-12-15 21:30:41 +0500

    Fetch Data of Countries using XML

---
+++ script.js
@@ -0,0 +1,35 @@
+'use strict';
+
+const btn = document.querySelector('.btn-country');
+const countriesContainer = document.querySelector('.countries');
+
+///////////////////////////////////////
+
+const request = new XMLHttpRequest();
+request.open('GET', 'https://restcountries.com/v3.1/name/pakistan');
+request.send();
+//Here we send request
+
+//Now we use callback and higher order function
+//Now this task is doing in bg, when it is loaded
+//this callback function called
+//this is request object
+request.addEventListener('load', function () {
+  //   console.log(this.responseText);
+  const [data] = JSON.parse(this.responseText);
+  console.log(data);
+  const html = `
+<article class="country">
+          <img class="country__img" src="${data.flags.svg}" />
+          <div class="country__data">
+            <h3 class="country__name">${data.name.common}</h3>
+            <h4 class="country__region">${data.region}</h4>
+            <p class="country__row"><span>👫</span>${data.population}</p>
+            <p class="country__row"><span>🗣️</span>${data.languages.eng}</p>
+            <p class="country__row"><span>💰</span>${data.currencies.PKR.name}</p>
+          </div>
+        </article>
+`;
+  countriesContainer.insertAdjacentHTML('beforeend', html);
+  countriesContainer.style.opacity = 1;
+});
